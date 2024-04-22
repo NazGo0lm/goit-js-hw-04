1e701ff3233674bd77dec89a7eeb60285ae1e11b
Nazar Nazarko <starmok20@gmail.com>
2024-04-22 21:05:00 +0300

    add js files

---
+++ js/task-2.js
@@ -0,0 +1,55 @@
+function calcAverageCalories(days){
+    let theArray = Object.values(days);
+    let theSum = 0;
+    for (let theValue of theArray) {
+        //console.log(theValue.calories);
+        //console.log(theArray.length);
+        let supValue = theValue.calories;
+        theSum += supValue
+    }
+    if (theArray.length >0) {
+        return theSum/theArray.length
+    }
+    return 0
+}
+
+
+
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
+
+/* console.log(
+  calcAverageCalories([{ day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },])
+); // 2155
+console.log(
+  calcAverageCalories([{ day: "monday", calories: 2040 },
+    ])
+); // 2040 */
